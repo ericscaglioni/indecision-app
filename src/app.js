3ec7e6b27457897184820703f45b1435ccb33e6f
Eric Scaglioni Ferreira <eric.ferreira@midassolutions.com.br>
2018-08-29 08:05:06 -0300

    Exploring JSX

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
-
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>
-var appRoot = document.getElementById('app');
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some paragraph</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
 
-ReactDOM.render(template, appRoot);
+var templateTwo = (
+    <div>
+        <h1>Eric Scaglioni</h1>
+        <p>Age: 27</p>
+        <p>Location: Brazil</p>
+    </div>
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot); 
